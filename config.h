c4c41e4f702657152f7322d4b05ff0c92c82862e
Charbel Sarkis <charbelsako@gmail.com>
2020-11-08 19:06:52 +0200

    Added rules and changed font to font awesome

--- config.h
+++ config.h
@@ -15,7 +15,7 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10", "FontAwesomePro5:size=10" };
+static const char *fonts[]          = { /*"monospace:size=10",*/ "Font Awesome:size=10", "Symbola:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -39,6 +39,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "mpv",  NULL,	  	  NULL,	      1 << 1,	    0,		 -1}	 
 };
 
 /* layout(s) */
@@ -73,11 +74,13 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *rofi[] = { "rofi", "-show", "run",  NULL };
-static const char *termcmd[]  = { "gnome-terminal", NULL };
+static const char *termcmd[]  = { "st", NULL };
 static const char *lock[] = {"lock.sh", NULL};
+static const char *firefox[] = {"firefox"};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+	{ MODKEY,			XK_w,	   spawn, 	   {.v = firefox } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = rofi } },
 	{ MODKEY|ShiftMask, 		XK_x, 	   spawn,	   { .v = lock } },
 	{ MODKEY,             XK_Return, spawn,          {.v = termcmd } },
